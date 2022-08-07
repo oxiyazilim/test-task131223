cc60c567f7ed6a217746b011ceda3994b6e92e43
Kenan Çelik <kenan@oxiyazilim.com>
2022-08-08 01:05:29 +0300

    fix menu hover

--- resources/views/themes/tailwind/menus/authenticated.blade.php
+++ resources/views/themes/tailwind/menus/authenticated.blade.php
@@ -47,7 +47,7 @@
             </div>
         </div>
         <a href="#" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">Support</a>
-        <a href="{{ route('wave.stocks') }}" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">Stocks</a>
+        <a href="{{ route('wave.stocks') }}" class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none border-b-2 border-transparent @if(Request::is('stocks')){{ 'border-b-2 border-indigo-500 text-gray-900 focus:border-indigo-700' }}@else{{ 'text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:text-gray-700 focus:border-gray-300' }}@endif">Stocks</a>
     </div>
 
 
